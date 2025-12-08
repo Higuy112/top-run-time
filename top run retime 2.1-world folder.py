ffc3f3e3308489206c143b5627a235956e5950ea
Higuy <realhiguy112@gmail.com>
2025-12-08 15:42:19 -0800

    Update to newer version

--- top run retime 2.1-world folder.py
+++ top run retime 2.1-world folder.py
@@ -2,8 +2,7 @@ import csv
 import easygui
 import datetime
 
-#modication of meera's top run retimer script to work with SpeedrunIGT versions 7.2.3/7.2.4
-#love from salix
+#modication of meera's top run retimer script to work with SpeedrunIGT versions 8+
 
 print('key:')
 print('scp - settings change pause')
@@ -11,6 +10,9 @@ print('dlp - dimension load pause')
 print('tdlp - timed dimension load pause')
 print('np - normal pause')
 print('up - untimed pause')
+print('\n')
+print('this tool is designed to work with version 8+ logs')
+print('if the timer version is 7.2.4 or lower, user the other retimer')
 print('\n\n')
 
 def spaces(n):
@@ -34,61 +36,42 @@ def decider(line):
         user what type of pause it is. returns pause
         length and inputted pause type'''
 
-    if altformat == True:
-        #for version 7.2.3 and 7.2.4 logs
-        #salix was here also
-        ptype = line[-2].split()
-        plength = line[1].split()
-        plength = plength[-2]
-        plength = int(plength[1:]) / 1000 #remove brackets and convert to seconds
+    #extract the needed information from the line
+    pnum = str(line[0].split()[0])
+    igt = line[0].split()[1]
+    startrta = line[0].split()[2]
+    endrta = line[0].split()[3]
+    plength = float(line[0].split()[4])
+    ptype = line[-2].split()
 
-        rta = str(line[1].split()[1])
-        startrta = rta[0:8]
-        endrta = rta[10:18]
-        igt = str(line[0].split()[2])
+    if ptype[-1] == 'player':
+        print('for pause', pnum)
+        print('from', startrta, 'to', endrta, 'at', igt, 'IGT')
+        ptype = input('enter type of pause: ')
+        while ptype not in ['scp', 'dlp', 'tdlp', 'np', 'up']:
+            ptype = input('try again champ: ')
 
-        if ptype[-1] == 'player':
-            print('for pause', line[0][:-15])
-            print('from', startrta, 'to', endrta, 'at', igt, 'IGT')
-            ptype = input('enter type of pause: ')
-            while ptype not in ['scp', 'dlp', 'tdlp', 'np', 'up']:
-                ptype = input('try again champ: ')
+    elif ptype[-1] == 'dimension' or ptype[-1] == 'load?' or ptype[-1] == 'ticks':
+        print('the time from', startrta, 'to', endrta, 'is a dimension load', pnum)
+        ptype = 'dl'
 
-        elif ptype[-1] == 'dimension':
-            print('the time from', startrta, 'to', endrta, 'is a dimension load', line[0][:-15])
-            ptype = 'dl'
-            
-        else:
-            print(line)
-            input('tell salix about this smth weird happened')
-            quit()
-            
-        newl = ptype+spaces(len(ptype))+startrta+'      '+endrta+'      '+(str(datetime.timedelta(seconds=plength)))[2:-3]+'      '
-
-    else:
-        ptype = line[-1].split()
-        plength = converttosecs(ptype[0])
+    elif ptype[-1] == 'world':
+        print('relog incident')
+        print(startrta, 'to', endrta, 'counted as dimension load', pnum, ', add back any penalty manually')
+        ptype = 'dl'
         
-        if ptype[-1] == 'player)':
-            print('for pause', line[0][:-15])
-            print('from', line[1], 'to', line[2], 'at', line[0].split()[1], 'IGT')
-            ptype = input('enter type of pause: ')
-            while ptype not in ['scp', 'dlp', 'tdlp', 'np', 'up']:
-                ptype = input('try again champ: ')
-            
-        elif ptype[-1] == 'dimension)':
-            print('the time from', line[1], 'to', line[2], 'is a dimension load', line[0][:-15])
-            ptype = 'dl'
-        else:
-            print(line)
-            input('tell meera about this smth weird happened')
-            quit()
-
-        newl = ptype+spaces(len(ptype))+str(line[1].split()[0])+'      '+str(line[2].split()[0])+'      '+str(line[-1].split()[0])+'      '
+    else:
+        print(line)
+        print('tell salix about this smth weird happened')
+        input('probably not my fault tho')
+        quit()
+        
+    newl = ptype+spaces(len(ptype))+startrta+'      '+endrta+'      '+"%.3f" % plength+'          '
 
     return plength, ptype, newl
 
-fpath = easygui.fileopenbox()
+fdir = easygui.diropenbox()
+fpath = f'{fdir}\\speedrunigt\\logs\\igt_timer.log'
 with open(fpath, newline='') as f:
     r = csv.reader(f)
     file = list(r)
@@ -98,28 +81,16 @@ with open(fpath, newline='') as f:
 
 newf = ['type     start RTA      end RTA        length         time added\n']
 
-#if timer is version 7.2.3 or 7.2.4 has a different log format so check for that
-#salix was here
-timerversion = file[1][0].split()
-timerversion = timerversion[-1].split('+')
-timerversion = timerversion[0]
-if timerversion == '7.2.3':
-    altformat = True
-elif timerversion == '7.2.4':
-    altformat = True
-else:
-    altformat = False
+file = file[1:]
 
-file = file[5:]
-
-igt = file[-1][0].split()
+igt = file[-5][0].split()
 igt = converttosecs(igt[-1])
 print('starting igt =', igt, 'seconds')
 print()
 
 
-nline = 5
-for line in file[:-1]:
+nline = 1
+for line in file[:-6]:
     plength, ptype, newl = decider(line)
 
     if ptype == 'scp':
@@ -164,10 +135,11 @@ ftime = (str(datetime.timedelta(seconds=igt)))[:-3]
 print(ftime)
 
 newf.append('')
-newf.append(origfile[-1])
+newf.append(origfile[-5])
 newf.append('retimed IGT for top run retiming to '+ftime)
 #print(newf)
-with open('topretimed.txt', 'w') as f:
+fname = input('enter name for output file: ')
+with open(fname+'.txt', 'w') as f:
     for line in newf:
         f.write(line+'\n')
 
